f699337bf135759ce6f19922577d3858d1bc88b9
Ian <1984therocker@gmail.com>
2021-08-25 20:57:53 -0500

    names added to notes column

---
+++ db/migrate/20210826004615_add_name_to_notes.rb
@@ -0,0 +1,5 @@
+class AddNameToNotes < ActiveRecord::Migration[6.1]
+  def change
+    add_column :notes, :name, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@ Visit.create!({ therapy: "Physical" })
 Client.create!({ name: "Nathan Taylor", visit_id: 1, goals: "Working on 'th' and 'sh' sounds", trials: "++ for 'th', + for 'sh'." })
 Client.create!({ name: "Daniel Deuel", visit_id: 2, goals: "Work on handwriting", trials: "Word 1: 'ball' ++, Word 2: 'stretch' --, Word 3: 'juice' -" })
 
-Note.create!({ client_id: 1, date: "7/23/2021", content: "Nathan did very well. He coninues to improve each week." })
-Note.create!({ client_id: 2, date: "7/25/2021", content: "Daniel did OK. He coninues to improve on his skills." })
+Note.create!({ client_id: 1, date: "7/23/2021", content: "Nathan did very well. He coninues to improve each week.", name: "Nathan Taylor" })
+Note.create!({ client_id: 2, date: "7/25/2021", content: "Daniel did OK. He coninues to improve on his skills.", name: "Daniel Deuel" })
